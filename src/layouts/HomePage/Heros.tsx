6a4fbd64ea988d38a61eb90c614f4035317e0e9b
Hemalatha <hemachowdary2744@gmail.com>
2023-10-03 14:20:58 +0530

    Carousel done

---
+++ src/layouts/HomePage/Heros.tsx
@@ -0,0 +1,20 @@
+export const Heros = () => {
+    return (
+        <div>
+            <div className="d-none d-lg-block">
+                <div className="row g-0 mt-5">
+                    <div className="col-image-left"></div>
+                </div>
+            </div>
+            <div className="col-4 col-md-4 container d-flex justify-content-center align-items-center">
+                <div className="ml-2"></div>
+                <h1>What have you been reading?</h1>
+                <p className="lead">
+                    The library team would love to know what you have been reading.
+                    Whether it is to learn a new skill or grow within one,
+                    we will be able to provide the top content for you!
+                </p>
+            </div>
+        </div>
+    );
+}
