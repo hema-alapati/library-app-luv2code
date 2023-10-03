c31c46ed45ff397d1c89317e229ab44077df4d1d
Hemalatha <hemachowdary2744@gmail.com>
2023-10-03 16:34:31 +0530

    Home Page Done

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,15 @@ import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
-import { ExploreTopBooks } from './layouts/HomePage/components/ExploreTopBooks';
-import { Carousel } from './layouts/HomePage/Carousel';
-import { Heros } from './layouts/HomePage/components/Heros';
-import { LibaryServices } from './layouts/HomePage/components/LibraryServices';
+import { HomePage } from './layouts/HomePage/HomePage';
 
-
-function App() {
+export const App = () => {
   return (
     <div>
       <Navbar />
-      <ExploreTopBooks />
-      <Carousel />
-      <Heros />
-      <LibaryServices />
+      <HomePage />
       <Footer />
     </div>
   );
 }
 
-export default App;
