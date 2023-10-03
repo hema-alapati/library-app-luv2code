266bc20f89b8a26d8784d4e68ed7dc50c7157345
Hemalatha <hemachowdary2744@gmail.com>
2023-10-03 15:04:22 +0530

    alignment disturbed

--- src/layouts/HomePage/Heros.tsx
+++ src/layouts/HomePage/Heros.tsx
@@ -8,14 +8,15 @@ export const Heros = () => {
                     </div>
                 </div>
                 <div className="col-4 col-md-4 container d-flex justify-content-center align-items-center">
-                    <div className="ml-2"></div>
-                    <h1>What have you been reading?</h1>
-                    <p className="lead">
-                        The library team would love to know what you have been reading.
-                        Whether it is to learn a new skill or grow within one,
-                        we will be able to provide the top content for you!
-                    </p>
-                    <a className="btn main-color btn-lg text-white" href="#">Sign up</a>
+                    <div className="ml-2">
+                        <h1>What have you been reading?</h1>
+                        <p className="lead">
+                            The library team would love to know what you have been reading.
+                            Whether it is to learn a new skill or grow within one,
+                            we will be able to provide the top content for you!
+                        </p>
+                        <a className="btn main-color btn-lg text-white" href="#">Sign up</a>
+                    </div>
                 </div>
                 <div className="row g-0">
                     <div className="col-4 col-md-4 container d-flex container 
@@ -36,6 +37,33 @@ export const Heros = () => {
                 </div>
             </div>
             {/* Mobile Heros */}
+            <div className="d-lg-none">
+                <div className="container">
+                    <div className="m-2">
+                        <div className="col-image-left"></div>
+                        <div className="mt-2">
+                            <h1>What have you been reading?</h1>
+                            <p className="lead">
+                                The library team would love to know what you have been reading.
+                                Whether it is to learn a new skill or grow within one,
+                                we will be able to provide the top content for you!
+                            </p>
+                            <a className="btn main-color btn-lg text-white" href="#">Sign up</a>
+                        </div>
+                    </div>
+                    <div className="m-2">
+                        <div className="col-image-right"></div>
+                        <div className="mt-2">
+                            <h1>What have you been reading?</h1>
+                            <p className="lead">
+                                The library team would love to know what you have been reading.
+                                Whether it is to learn a new skill or grow within one,
+                                we will be able to provide the top content for you!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
