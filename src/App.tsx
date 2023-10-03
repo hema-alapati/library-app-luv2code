971bf88b164c113633d997f5adb05dcceadb6f7c
Hemalatha <hemachowdary2744@gmail.com>
2023-10-03 16:17:22 +0530

    Added Heros and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
 import { Carousel } from './layouts/HomePage/Carousel';
 import { Heros } from './layouts/HomePage/Heros';
+import { LibaryServices } from './layouts/HomePage/LibraryServices';
+
 
 function App() {
   return (
@@ -12,6 +15,8 @@ function App() {
       <ExploreTopBooks />
       <Carousel />
       <Heros />
+      <LibaryServices />
+      <Footer />
     </div>
   );
 }
